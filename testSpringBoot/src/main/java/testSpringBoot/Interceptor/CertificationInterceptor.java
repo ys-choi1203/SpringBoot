630e3a7896e2da4cccc5db9372e7d1cf42417523
soldesk <soldesk@DESKTOP-ADVJLNP>
2020-08-27 17:33:01 +0900

    last commit

--- testSpringBoot/src/main/java/testSpringBoot/Interceptor/CertificationInterceptor.java
+++ testSpringBoot/src/main/java/testSpringBoot/Interceptor/CertificationInterceptor.java
@@ -11,7 +11,9 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import testSpringBoot.command.AuthInfo;
 
 public class CertificationInterceptor extends HandlerInterceptorAdapter{
-	// 컨
+	//컨트롤러(즉 RequestMapping이 선언된 메서드 진입) 실행 직전에 동작.
+	// 반환 값이 true일 경우 정상적으로 진행이 되고, false일 경우 실행이 멈춥니다.(컨트롤러 진입을 하지 않음)
+	// 전달인자 중 Object handler는 핸들러 매핑이 찾은 컨트롤러 클래스 객체입니다.
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
@@ -25,6 +27,8 @@ public class CertificationInterceptor extends HandlerInterceptorAdapter{
 		}
 	}
 	
+	// 컨트롤러 진입 후 view가 랜더링 되기 전 수행이 됩니다.
+	// 전달인자의 modelAndView을 통해 화면 단에 들어가는 데이터 등의 조작이 가능합니다.
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
@@ -32,7 +36,7 @@ public class CertificationInterceptor extends HandlerInterceptorAdapter{
 		super.postHandle(request, response, handler, modelAndView);
 	}
 	
-	// 컨트롤러 진입 후 view가 정상적으로 
+	//컨트롤러 진입 후 view가 정상적으로 랜더링 된 후 제일 마지막에 실행이 되는 메서드입니다.
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
